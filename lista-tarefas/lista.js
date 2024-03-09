697cb9b9a866aa39d2fb7f1576205d17d4666a6e
Julia Barbosa <juiolanda@hotmail.com>
2024-03-08 22:55:53 -0300

    estilização adicional

--- lista-tarefas/lista.js
+++ lista-tarefas/lista.js
@@ -1,7 +1,7 @@
 // Função para adicionar uma nova tarefa
 function addTask() {
-    var input = document.getElementById("taskInput");
-    var taskText = input.value.trim(); // Remove espaços em branco antes e depois do texto
+    let input = document.getElementById("taskInput");
+    let taskText = input.value.trim(); // Remove espaços em branco antes e depois do texto
 
     if (taskText === "") {
         alert("Por favor, insira uma tarefa.");
@@ -9,21 +9,25 @@ function addTask() {
     }
 
     // Criar um novo item de lista
-    var listItem = document.createElement("li");
+    let listItem = document.createElement("li");
+    listItem.classList = 'task-item';
 
     // Adicionar texto da tarefa
     listItem.textContent = taskText;
 
     // Adicionar botão de conclusão
-    var completeButton = document.createElement("button");
+    let completeButton = document.createElement("button");
+    completeButton.classList = 'button-complete';
     completeButton.textContent = "Concluir";
+
     completeButton.onclick = function () {
         toggleTaskComplete(listItem);
     };
     listItem.appendChild(completeButton);
 
     // Adicionar botão de exclusão
-    var deleteButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+    deleteButton.classList = 'button-delete';
     deleteButton.textContent = "Excluir";
     deleteButton.onclick = function () {
         deleteTask(listItem);
